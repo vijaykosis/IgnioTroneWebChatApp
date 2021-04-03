0788965742dbca4dc3eb7a67e84e89b8e68e9a87
Vijay <vijaykoshis@gmail.com>
2021-04-04 01:39:12 +0530

    Add files via upload
    initial commit

---
+++ src/main/java/com/igniotrone/chatapplication/IgnioTroneChatapplicationApplication.java
@@ -0,0 +1,12 @@
+package com.igniotrone.chatapplication;
+
+import org.springframework.boot.SpringApplication;
+		import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgnioTroneChatapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgnioTroneChatapplicationApplication.class, args);
+	}
+}
